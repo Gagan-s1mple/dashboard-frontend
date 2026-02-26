9078d4633c3fd9cb1abaeddb8626b96e6b20fb8b
Pavan Anekar <pavananekar7@gmail.com>
2026-02-26 10:46:04 +0530

    added ssl certified url

--- my-app/src/proxy.ts
+++ my-app/src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const fastapi_path = "http://34.233.120.188:8000";
+const fastapi_path = "https://54-85-180-205.sslip.io";
 
 export function proxy(request: NextRequest) {
   const { pathname, search } = request.nextUrl;
